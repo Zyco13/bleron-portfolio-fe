09c348ee9ec97e5f546033b421eeb59aa9d47bbe
Zyco13 <bleronzekaj@icloud.com>
2026-05-10 17:52:24 +0200

    update api

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -11,11 +11,11 @@ export default function Contact() {
  const handleSubmit = async (e: React.FormEvent) => {
   e.preventDefault()
 
-  const response = await fetch('http://localhost:8080/api/contact', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ name, email, message }),
-  })
+ const response = await fetch('https://bleron-portfolio-be-production.up.railway.app/api/contact', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({ name, email, message }),
+})
 
   if (response.ok) {
     setSent(true)
